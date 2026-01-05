14888bf3cbb37c6d5e6b501980ea69b3ae49ae51
buraaks <buraktemur0816@gmail.com>
2026-01-05 10:30:31 +0300

    Add AnimalSpecies enum and sync UI models

---
+++ BarnManagement.Core/Enums/AnimalSpecies.cs
@@ -0,0 +1,8 @@
+namespace BarnManagement.Core.Enums;
+
+public enum AnimalSpecies
+{
+    Cow = 1,
+    Sheep = 2,
+    Chicken = 3
+}
---
+++ BarnManagement.UI/Models/AnimalSpecies.cs
@@ -0,0 +1,8 @@
+namespace BarnManagement.UI.Models;
+
+public enum AnimalSpecies
+{
+    Cow = 1,
+    Sheep = 2,
+    Chicken = 3
+}
